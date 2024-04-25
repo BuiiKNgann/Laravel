c0d87b72a5e163a69c225f44f2bcc58633185d78
BuiiKNgann <buikimngan22130@gmail.com>
2024-04-25 23:51:12 +0700

    mylaravel

---
+++ app/Http/Controllers/admin/CategoryController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Category;
+use App\Models\TempImage;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\File;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
+
+class CategoryController extends Controller
+{
+    public function index(Request $request)
+    {
+        $categories = Category::latest();
+        if (!empty($request->get('keyword'))) {
+            $categories = $categories->where('name', 'like', '%' . $request->get('keyword') . '%');
+        }
+        $categories = $categories->paginate(10);
+
+        return view('admin.category.list', compact('categories'));
+    }
+    public function create()
+    {
+        return view('admin.category.create');
+    }
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'slug' => 'required|unique:categories',
+
+        ]);
+        if ($validator->passes()) {
+            $category = new Category();
+            $category->name = $request->name;
+            $category->slug = $request->slug;
+            $category->status = $request->status;
+            $category->save();
+            //save image here
+            if (!empty($request->image_id)) {
+                $tempImage = TempImage::find($request->image_id);
+                $extArray = explode('.', $tempImage->name);
+                $ext = last($extArray);
+
+                $newImageName = $category->id . '.' . $ext;
+                $sPath = public_path() . '/temp/' . $tempImage->name;
+                $dPath = public_path() . '/uploads/category' . $newImageName;
+                File::copy($sPath, $dPath);
+                // Generate Image Thumbnaul
+                $dPath = public_path() . '/uploads/category/thumb/' . $newImageName;
+                $manager = new ImageManager(new Driver());
+                $image = $manager->read($sPath);
+                $image->scale(width: 450);
+                $image->save($dPath);
+                $category->image = $newImageName;
+                $category->save();
+            }
+
+
+
+
+            session()->flash('success', 'Category added successfully');
+
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Category added successfully'
+            ]);
+        } else {
+            return response()->json([
+                'status' => false,
+                'errors' => $validator->errors()
+            ]);
+        }
+    }
+    public function edit()
+    {
+    }
+    public function update()
+    {
+    }
+    public function destroy()
+    {
+    }
+}
